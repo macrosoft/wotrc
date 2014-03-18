3e65b658c2ec7de26e069b546819d4486c3a91a7
Zagidulin Artyom <p0chta@yandex.ru>
2014-03-18 21:29:33 +0400

    test replace camo

--- src/randcamo.py
+++ src/randcamo.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import BigWorld
+import items.vehicles
+from VehicleAppearance import VehicleAppearance
 from debug_utils import *
 
-LOG_NOTE("Hello from Rand camo!")
+old_va_prerequisites = VehicleAppearance._VehicleAppearance__getCamouflageParams
+def new_va_prerequisites(self, vehicle):
+    vDesc = vehicle.typeDescriptor
+    result = old_va_prerequisites(self, vehicle)
+    LOG_NOTE(vDesc.camouflages[0])
+    return (3, 1394280840, 7)
+
+VehicleAppearance._VehicleAppearance__getCamouflageParams = new_va_prerequisites
+
