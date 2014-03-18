3d7987d505f2996e8397a6e642163f7d7be13d8e
Zagidulin Artyom <p0chta@yandex.ru>
2014-03-18 23:02:13 +0400

    first work versiton

--- src/randcamo.py
+++ src/randcamo.py
@@ -2,15 +2,18 @@
 # -*- coding: utf-8 -*-
 import BigWorld
 import items.vehicles
+import random
+import time
 from VehicleAppearance import VehicleAppearance
 from debug_utils import *
 
 old_va_prerequisites = VehicleAppearance._VehicleAppearance__getCamouflageParams
 def new_va_prerequisites(self, vehicle):
     vDesc = vehicle.typeDescriptor
-    result = old_va_prerequisites(self, vehicle)
-    LOG_NOTE(vDesc.camouflages[0])
-    return (3, 1394280840, 7)
+    customization = items.vehicles.g_cache.customization(vDesc.type.customizationNationID)
+    camouflages = customization['camouflages'].keys()
+    camouflages.append(None)
+    return (random.choice(camouflages), int(time.time()), 7)
 
 VehicleAppearance._VehicleAppearance__getCamouflageParams = new_va_prerequisites
 
