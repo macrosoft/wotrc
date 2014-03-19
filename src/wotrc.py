46ff8db7420cf3c5f76af186483f8dcbc700982c
Zagidulin Artyom <p0chta@yandex.ru>
2014-03-19 19:37:25 +0400

    save existing camo

--- src/wotrc.py
+++ src/wotrc.py
@@ -2,22 +2,22 @@
 # -*- coding: utf-8 -*-
 import BigWorld
 import items.vehicles
-import random
 import time
 from VehicleAppearance import VehicleAppearance
 from debug_utils import *
 
-old_va_prerequisites = VehicleAppearance._VehicleAppearance__getCamouflageParams
+old_va_getCamouflageParams = VehicleAppearance._VehicleAppearance__getCamouflageParams
 
-def new_va_prerequisites(self, vehicle):
+def new_va_getCamouflageParams(self, vehicle):
+    result = old_va_getCamouflageParams(self, vehicle)
+    if result[0] is not None:
+        return result
     vDesc = vehicle.typeDescriptor
     customization = items.vehicles.g_cache.customization(vDesc.type.customizationNationID)
     camouflages = customization['camouflages'].keys()
     camouflages.append(None)
-    camouflageId = (BigWorld.player().arena.arenaUniqueID + vehicle.id) % len(camouflages)\
-        if hasattr(BigWorld.player(), 'arena') \
-        else random.randint(0, len(camouflages) - 1)
+    camouflageId = vehicle.id % len(camouflages)
     return (camouflages[camouflageId], int(time.time()), 7)
 
-VehicleAppearance._VehicleAppearance__getCamouflageParams = new_va_prerequisites
+VehicleAppearance._VehicleAppearance__getCamouflageParams = new_va_getCamouflageParams
 
