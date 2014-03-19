32d553d0e63809de0b32349fdd5c6c8d8970f0cd
Zagidulin Artyom <p0chta@yandex.ru>
2014-03-19 21:46:35 +0400

    random camo in hangar

--- src/wotrc.py
+++ src/wotrc.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 import BigWorld
 import items.vehicles
+import random
 import time
+from gui import g_tankActiveCamouflage
+from gui.ClientHangarSpace import ClientHangarSpace
 from VehicleAppearance import VehicleAppearance
 from debug_utils import *
 
@@ -26,3 +29,15 @@ def new_va_getCamouflageParams(self, vehicle):
 
 VehicleAppearance._VehicleAppearance__getCamouflageParams = new_va_getCamouflageParams
 
+old_cs_recreateVehicle = ClientHangarSpace.recreateVehicle
+
+def new_cs_recreateVehicle(self, vDesc, vState, onVehicleLoadedCallback = None):
+    customization = items.vehicles.g_cache.customization(vDesc.type.customizationNationID)
+    camouflages = customization['camouflages'].keys()
+    camouflages.append(None)
+    vDesc.camouflages = ((random.choice(camouflages), int(time.time()), 7), \
+        (random.choice(camouflages), int(time.time()), 7), \
+        (random.choice(camouflages), int(time.time()), 7))
+    old_cs_recreateVehicle(self, vDesc, vState, onVehicleLoadedCallback)
+
+ClientHangarSpace.recreateVehicle = new_cs_recreateVehicle
