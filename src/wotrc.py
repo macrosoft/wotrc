ac6f2087af9226139d37d2b1932e55bda87db827
Zagidulin Artyom <p0chta@yandex.ru>
2014-03-23 11:41:39 +0400

    change camouflage only after battle

--- src/wotrc.py
+++ src/wotrc.py
@@ -6,13 +6,14 @@ import os
 import json
 import random
 import time
+from Account import Account
 from gui import g_tankActiveCamouflage
 from gui.ClientHangarSpace import ClientHangarSpace
 from VehicleAppearance import VehicleAppearance
 from xml.dom import minidom
 from debug_utils import *
 
-class Config(object):
+class Wotrc(object):
 
     def __init__(self):
         path_items = minidom.parse(os.path.join(os.getcwd(), 'paths.xml')).getElementsByTagName('Path')
@@ -25,6 +26,15 @@ class Config(object):
                         jsConfig = json.load(data_file)
                         break
         self.remap = jsConfig.get('remap', {})
+        self.hangarCamo = {}
+
+old_onBecomeNonPlayer = Account.onBecomeNonPlayer
+
+def new_onBecomeNonPlayer(self):
+    old_onBecomeNonPlayer(self)
+    wotrc.hangarCamo.clear()
+
+Account.onBecomeNonPlayer = new_onBecomeNonPlayer
 
 old_va_getCamouflageParams = VehicleAppearance._VehicleAppearance__getCamouflageParams
 
@@ -39,7 +49,7 @@ def new_va_getCamouflageParams(self, vehicle):
     camouflages = []
     for key in customization['camouflages']:
         camuflage = customization['camouflages'][key]
-        kind = cfg.remap.get(camuflage['texture'], camuflage['kind'])
+        kind = wotrc.remap.get(camuflage['texture'], camuflage['kind'])
         if kind == camouflageKind:
             camouflages.append(key)
     camouflages.append(None)
@@ -51,18 +61,22 @@ VehicleAppearance._VehicleAppearance__getCamouflageParams = new_va_getCamouflage
 old_cs_recreateVehicle = ClientHangarSpace.recreateVehicle
 
 def new_cs_recreateVehicle(self, vDesc, vState, onVehicleLoadedCallback = None):
-    customization = items.vehicles.g_cache.customization(vDesc.type.customizationNationID)
-    camouflages = customization['camouflages'].keys()
-    camouflages.append(None)
-    tmpCamouflages = []
-    for i in range(0, 3):
-        if vDesc.camouflages[i][0] is not None:
-            tmpCamouflages.append(vDesc.camouflages[i])
-        else:
-            tmpCamouflages.append((random.choice(camouflages), int(time.time()), 7))
-    vDesc.camouflages = tuple(tmpCamouflages)
+    if wotrc.hangarCamo.has_key(vDesc.type.compactDescr):
+        vDesc.camouflages = wotrc.hangarCamo[vDesc.type.compactDescr]
+    else:
+        customization = items.vehicles.g_cache.customization(vDesc.type.customizationNationID)
+        camouflages = customization['camouflages'].keys()
+        camouflages.append(None)
+        tmpCamouflages = []
+        for i in range(0, 3):
+            if vDesc.camouflages[i][0] is not None:
+                tmpCamouflages.append(vDesc.camouflages[i])
+            else:
+                tmpCamouflages.append((random.choice(camouflages), int(time.time()), 7))
+        vDesc.camouflages = tuple(tmpCamouflages)
+        wotrc.hangarCamo[vDesc.type.compactDescr] = tuple(tmpCamouflages)
     old_cs_recreateVehicle(self, vDesc, vState, onVehicleLoadedCallback)
 
 ClientHangarSpace.recreateVehicle = new_cs_recreateVehicle
 
-cfg = Config()
+wotrc = Wotrc()
